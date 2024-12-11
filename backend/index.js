4b531e1b4eeff22f476da935099740f93c698f3b
Dan Perkins <danjonperkins@gmail.com>
2024-12-11 16:00:49 +0000

    expressjs initial setup

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/routes");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Database connection
+const mongoURI = process.env.MONGO_URI;
+mongoose.connect(mongoURI);
+const database = mongoose.connection;
+
+database.on("error", (error) => console.error(error));
+database.once("open", () => console.log("Connected to database"));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.use("/api", routes);
